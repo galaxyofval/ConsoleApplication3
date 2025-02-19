c9b0a54ca61cdbd92c930138b6ecdd0c5820dbc9
galaxyofval <153199753+galaxyofval@users.noreply.github.com>
2025-02-19 17:03:54 -0500

    Update ConsoleApplication3.cpp
    Fixed typo

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -12,13 +12,13 @@ int main()
     float percentageOfCitrusEnjoyers; // Shows how many prefer citrus over any other flavor.
 
     // Ask the user a few questions to get the numbers the program needs.
-    cout << "How many people have you recently served?" << endl;
+    cout << "How many people have you recently surveyed?" << endl;
     cin >> customerTotal;
     cout << "What percentage of customers bought drinks in the past week?" << endl;
     cin >> percentageOfBuyers;
     cout << "Of those customers, what percentage of customers enjoyed citrus more than any other flavor?" << endl;
     cin >> percentageOfCitrusEnjoyers;
-    cout << "Of the " << customerTotal << " people served, " << round((customerTotal * (percentageOfBuyers / 100))) << " of them buy at least one energy drink every week." << endl; // Does the math to show the amount of people who have bought a drink in the past week.
+    cout << "Of the " << customerTotal << " people surveyed, " << round((customerTotal * (percentageOfBuyers / 100))) << " of them buy at least one energy drink every week." << endl; // Does the math to show the amount of people who have bought a drink in the past week.
     cout << "Out of the " << round((customerTotal * (percentageOfBuyers / 100))) << " that buy one or more drinks each week, " << round(((customerTotal * (percentageOfBuyers / 100)) * (percentageOfCitrusEnjoyers / 100))) << " like citrus flavor more than any other flavor." << endl; // Does the math to calculate the people who enjoy citrus flavors the most out of the people who buy at least one energy drink within the past week.
 
     return 0;
